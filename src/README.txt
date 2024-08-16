597b92a6114969812ae6b2ea3e06bf188c97046a
Davis Woodring <dwoodr10@wgu.edu>
2024-08-16 11:58:11 -0400

    Cleaned up the project, added some styles and data in the README.txt. Submission Soon.

--- src/README.txt
+++ src/README.txt
@@ -1,2 +1,28 @@
+D280 JavaScript Programming WGU
+Davis Woodring
+
 Angular Version: 18.1.3
 Student ID: 012288388
+
+Overview:
+
+Due to Angular having so many big changes with their versions over the years it took some time understanding what was
+needed to be learned vs discarded like with NGModule and Standalone but once I got deep into some tutorials and see how
+things were supposed to behave it started to come together and after 3 attempts I have an Interactive Map!
+(Along with a VanillaJS version). I am going to attempt to clean it up and do some more styling and get my file size
+down by eliminating any nonsense files.
+
+1st Build: Using Angular I could not get the data to display on the table and GitLab wasn't showing my pushes,
+           so after a while I decided to just restart and rebuild the project.
+
+2nd Build: I got Git to start pushing to Gitlab properly using IntelliJ IDEA but, and got my project to work, but I only
+           created the project with Angular and didn't build it with Angular just VanillaJS after talking to my CI.
+           my CI stated that the branch I was pushing to was okay despite it not being named properly he stated he was
+           more worried about the proper implementation and use of Angular.
+
+3rd Build: I used the same GitLab pipeline as my CI Chris Peters said it wouldn't be a problem with the branch named Master
+           and I didn't want to risk having the problem from my first build and fearing I wouldn't be able to push my steps
+           properly. Thankfully on my 3rd Build I was able to get my map interactive using Angular.
+
+Thanks for reviewing my project!
+Davis Woodring
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,14 +1,13 @@
 import { Component, inject, ElementRef  } from '@angular/core';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http"; // HHTPClientModule deprecated from new Angular Version
 import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule], // HHTPClientModule deprecated from new Angular Version
   templateUrl: './map.component.html',
 })
-
 export class MapComponent {
   name: any;
   capital: string;
@@ -18,7 +17,7 @@ export class MapComponent {
   latitude: string ;
   data: any= [];
   dataPath: any= [];
-  currCountryCode: string = 'ua';
+  currCountryCode: string = '';
   private httpClient = inject(HttpClient);
 
   constructor(private elementRef: ElementRef) {
@@ -29,7 +28,6 @@ export class MapComponent {
     this.longitude = '';
     this.latitude = '';
   }
-
 private ngAfterViewInit() {
     const svgElement = this.elementRef.nativeElement.querySelector('svg');
     svgElement.querySelectorAll('path').forEach((path: SVGPathElement) => {
@@ -42,7 +40,6 @@ private ngAfterViewInit() {
     this.currCountryCode = pathID;
     this.fetchData(this.currCountryCode);
   }
-
 private displayInfo(infoFromApi: any){
   this.name = infoFromApi[1][0].name;
   this.capital = infoFromApi[1][0].capitalCity;
@@ -51,7 +48,6 @@ private displayInfo(infoFromApi: any){
   this.longitude = infoFromApi[1][0].longitude;
   this.latitude = infoFromApi[1][0].latitude;
 }
-
   private fetchData(pathID: string): void {
     let url = 'https://api.worldbank.org/v2/country/'+this.currCountryCode+'?format=json';
     this.httpClient.get(url)
